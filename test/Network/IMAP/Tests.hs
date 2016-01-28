dd76554c2412093505541d6ea67bc7ba525dd5e3
Michal Kawalec <michal@monad.cat>
2016-01-28 23:53:48 +0100

    beginnings of a testing framework

---
+++ test/Network/IMAP/Tests.hs
@@ -0,0 +1,10 @@
+module Network.IMAP.Tests where
+
+import Network.IMAP
+
+import qualified Test.QuickCheck as QC
+import qualified Test.QuickCheck.Monadic as QC
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase)
+import Test.Tasty.QuickCheck (testProperty)
+import Test.HUnit (Assertion, (@?=))
---
+++ test/main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import System.Exit (exitFailure)
+import Network.IMAP.Tests
+
+main = putStrLn "fails" >> exitFailure
